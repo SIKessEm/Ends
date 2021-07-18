99e055daed6f7a6d73b8389b1dc4396819ac7842
SIGUI kessé Emmanuel <sikessem@omninf.com>
2021-07-18 15:43:45 +0200

    Use the script class

--- client/index.php
+++ client/index.php
@@ -1,3 +1,9 @@
-<?php
-echo 'Welcome to Ends';
-exit;
+<?php use Ends\Script;
+
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+$script = new Script('index');
+
+$script->put('Hello world !');
+
+$script->end();
